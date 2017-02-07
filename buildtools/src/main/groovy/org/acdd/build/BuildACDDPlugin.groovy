9bc1e42e5591b6f50e0afea76e7fda55df3ef81b
BunnyBlue <bunnyblueair@gmail.com>
2017-02-07 15:40:42 +0800

    fix windows path

--- buildtools/src/main/groovy/org/acdd/build/BuildACDDPlugin.groovy
+++ buildtools/src/main/groovy/org/acdd/build/BuildACDDPlugin.groovy
@@ -14,7 +14,8 @@ public class BuildACDDPlugin implements Plugin<Project>{
         project.task("acddPluginBuildTask") <<{
             String[] args = new String[3];
             args[0] = "${project.projectDir}/libs/armeabi".toString();
-            args[1] = "${project.projectDir}\\assets\\bundle-info.json".toString();
+            args[1] =project.projectDir.absolutePath+File.separator+"assets"+File.separator+"bundle-info.json";
+            //args[1] = "${project.projectDir}\assets\\bundle-info.json".toString();
             args[2] = "${project.projectDir}/.bundleCache".toString();
 
 //            println "apkFile: " + args[0]
